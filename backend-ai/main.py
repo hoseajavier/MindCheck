b74987acd00ce035f5a14eeafc03c4a4d9cd5856
Hosea Javier <hosea.j16@gmail.com>
2026-05-27 12:23:43 +0700

    MindCheck v1.0.0

---
+++ backend-ai/main.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+import joblib
+import numpy as np
+import pandas as pd
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load(
+    "stress_detection_random_forest.pkl"
+)
+
+features = [
+    "snoring_rate",
+    "respiration_rate",
+    "body_temperature",
+    "limb_movement",
+    "blood_oxygen",
+    "eye_movement",
+    "sleeping_hours",
+    "heart_rate"
+]
+
+class StressInput(BaseModel):
+
+    snoring_rate: float
+    respiration_rate: float
+    body_temperature: float
+    limb_movement: float
+    blood_oxygen: float
+    eye_movement: float
+    sleeping_hours: float
+    heart_rate: float
+
+label_map = {
+    0: "Rendah",
+    1: "Sedang",
+    2: "Tinggi"
+}
+
+@app.get("/")
+def root():
+
+    return {
+        "message": "Stress Detection API Running"
+    }
+    
+@app.post("/predict")
+def predict(data: StressInput):
+
+    try:
+
+        values_dict = data.dict()
+
+        values = pd.DataFrame(
+            [[values_dict[f] for f in features]],
+            columns=features
+        )
+
+        prediction = model.predict(values)[0]
+
+        probabilities = model.predict_proba(values)[0]
+
+        return {
+
+            "prediction": int(prediction),
+
+            "label": label_map[int(prediction)],
+
+            "probabilities": {
+                "rendah": round(probabilities[0] * 100, 2),
+                "sedang": round(probabilities[1] * 100, 2),
+                "tinggi": round(probabilities[2] * 100, 2),
+            }
+
+        }
+
+    except Exception as e:
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
