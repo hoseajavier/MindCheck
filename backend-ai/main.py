bd597486fb03e29073d94e4cbd70e492d17e37d2
Hosea Javier <hosea.j16@gmail.com>
2026-05-29 17:39:43 +0700

    fix main.py

--- backend-ai/main.py
+++ backend-ai/main.py
@@ -83,5 +83,5 @@ def predict(data: StressInput):
         )
         
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
